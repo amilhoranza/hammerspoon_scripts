03729bdd666f2eeea02f6d8b7aae1ea99b33d5ff
Andre Miglioranza <amilhoranza@gmail.com>
2025-01-05 11:30:07 -0300

    Add comments explaining Hammerspoon Spoons functionality
    - Describe FinderEnhancer's behavior for bringing Finder windows to front
    - Explain CaffeineMenu's purpose for preventing sleep and its menu bar item
    - Note ConfigReloader's automatic config reloading feature
    - Provide detailed explanation of ShiftIt's window management shortcuts
    - Include default keyboard shortcuts for various window operations in ShiftIt

--- init.lua
+++ init.lua
@@ -1,6 +1,34 @@
--- Load and start all Spoons
+-- FinderEnhancer: Brings all Finder windows to front when switching to Finder
+-- When you activate Finder, all its windows will automatically come to front
 hs.loadSpoon("FinderEnhancer"):start()
+
+-- CaffeineMenu: Prevents your Mac from going to sleep
+-- Adds a AWAKE/SLEEPY item to the menu bar that you can click to toggle
 hs.loadSpoon("CaffeineMenu"):start()
+
+-- ConfigReloader: Automatically reloads Hammerspoon config when changes are detected
+-- No shortcuts - works automatically in the background
 hs.loadSpoon("ConfigReloader"):start()
+
+-- ShiftIt: Window management with keyboard shortcuts
+-- Default shortcuts (ctrl + alt + cmd):
+--   left:  Move window to left half
+--   right: Move window to right half
+--   up:    Move window to top half
+--   down:  Move window to bottom half
+--   1:     Move window to top-left quarter
+--   2:     Move window to top-right quarter
+--   3:     Move window to bottom-left quarter
+--   4:     Move window to bottom-right quarter
+--   m:     Maximize window
+--   f:     Toggle full screen
+--   z:     Toggle zoom
+--   c:     Center window
+--   n:     Move to next screen
+--   p:     Move to previous screen
+--   =:     Resize window out
+--   -:     Resize window in
+--   a:     Arrange windows in 4 quarters
+--   t:     Arrange windows in 3 columns
 hs.loadSpoon("ShiftIt")
 spoon.ShiftIt:bindHotkeys({})
